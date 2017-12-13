f8ac93eda28f813afb7038f0ccfa63a28247652c
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2017-12-13 14:46:01 +0100

    Fix path name

--- builds/fccsw-build.sh
+++ builds/fccsw-build.sh
@@ -7,8 +7,8 @@ set -x
 touch $WORKSPACE/controlfile
 
 #---Set up environment----------------------------------------------------------------------
-cd $WORKSPACE/FCCSW
-source $WORKSPACE/FCCSW/init.sh
+cd $WORKSPACE/fccsw 
+source $WORKSPACE/fccsw/init.sh
 
 # TODO: afs reference needs to be removed
 cp /afs/cern.ch/exp/fcc/sw/0.5/testsamples/example_MyPythia.dat .
