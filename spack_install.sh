cfbaeaaf5899f1cb080b72db24245cc5e69daaba
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-03-15 12:05:29 +0100

    Problems with Spack regex expression

--- spack_install.sh
+++ spack_install.sh
@@ -142,11 +142,11 @@ if [[ "$viewpath" != "" && "$package" != "" ]]; then
 
   # Exclude fccsw
   if [[ "$package" == "fccstack" ]]; then
-    exceptions=$exceptions"\|fccsw"
+    exceptions=$exceptions"|fccsw"
   fi
 
   echo "Command: spack view -d true -e $exceptions symlink -i $viewpath $package/$pkghash"
-  spack view -d true -e $exceptions symlink $viewpath $package/$pkghash
+  spack view -d true -e "$exceptions" symlink $viewpath $package/$pkghash
   result=$(($result + $?))
 fi
 
