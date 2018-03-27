8a460473c5bd591826fe54a6e7a845773e3724a7
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-03-27 12:01:50 +0200

    Use compiler passed as parameter

--- spack_install.sh
+++ spack_install.sh
@@ -92,16 +92,16 @@ export HEP_SPACK=$SPACK_ROOT/var/spack/repos/hep-spack
 
 gcc49version=4.9.3
 gcc62version=6.2.0
-export COMPILERversion=${COMPILER}version
+export compilerversion=${compiler}version
 
 # Prepare defaults/linux configuration files (compilers and external packages)
-cat $THIS/config/compiler-slc6-${COMPILER}.yaml > $SPACK_CONFIG/linux/compilers.yaml
+cat $THIS/config/compiler-slc6-${compiler}.yaml > $SPACK_CONFIG/linux/compilers.yaml
 
 # Use a default patchelf installed in fcc.cern.ch
 cat $THIS/config/patchelf.yaml >> $SPACK_CONFIG/linux/packages.yaml
 
 # Use a default slc6 compiler taken from cvmfs/sft.cern.ch
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/${!COMPILERversion}binutils/x86_64-slc6/setup.sh
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/${!compilerversion}binutils/x86_64-slc6/setup.sh
 
 # Create mirrors.yaml to use local buildcache
 if [ "$buildcache" != "" ]; then
