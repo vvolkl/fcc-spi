ec835b32495fa7e5da2ed9c575ce8b8fdc60b722
SFT Nightlies <sftnight@lcgapp-slc6-physical1.cern.ch>
2019-02-21 16:36:05 +0100

    Add gcc7 support

--- spack_install.sh
+++ spack_install.sh
@@ -118,6 +118,8 @@ export HEP_SPACK=$SPACK_ROOT/var/spack/repos/hep-spack
 
 gcc49version=4.9.3
 gcc62version=6.2.0
+gcc73version=7.3.0
+
 export compilerversion=${compiler}version
 
 # Prepare defaults/linux configuration files (compilers and external packages)
