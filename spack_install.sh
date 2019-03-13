34fd257e094b8e6e928d42a2215ccd1fd6885ddc
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-03-13 14:47:11 +0100

    Remove name from the spec when creating view

--- spack_install.sh
+++ spack_install.sh
@@ -193,7 +193,7 @@ if [[ "$viewpath" != "" && "$package" != "" ]]; then
   fi
 
   echo "Command: spack view -d true -e $exceptions symlink -i $viewpath $package/$pkghash"
-  spack view -d true -e "$exceptions" symlink $viewpath $package/$pkghash
+  spack view -d true -e "$exceptions" symlink $viewpath /$pkghash
   viewcreated=$?
   result=$(($result + $viewcreated))
   if [ $viewcreated -eq 0 ];then
